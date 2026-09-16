ec8ad7b1ee4b530b9819393eaf6179e72b85d24f
Ravinjeet Singh <75482385+Ravin1010@users.noreply.github.com>
2026-09-16 15:07:43 +0000

    1

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def index():
 def main(): 
     return(render_template("main.html"))
 
+@app.route("/transferMoney", methods=["GET", "POST"])
+def transferMoney(): 
+    return(render_template("transferMoney.html"))
+
 if __name__ == "__main__": 
     app.run()
+
